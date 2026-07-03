4c2c1e2bec03f2d0c8c06584ade7c84822189f58
ramen-root <218883712+ramen-root@users.noreply.github.com>
2026-07-02 20:44:31 -0600

    fix: add widgets to Main.layoutManager.uiGroup (above the overview overlay) instead of _backgroundGroup.

--- extension/modules/backgroundWidgets/baseWidget.js
+++ extension/modules/backgroundWidgets/baseWidget.js
@@ -1,8 +1,61 @@
 'use strict';
 
 import GObject from 'gi://GObject';
+import GLib from 'gi://GLib';
 import St from 'gi://St';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import {WorkspaceBackground} from 'resource:///org/gnome/shell/ui/workspace.js';
+
+const _widgets = new Set();
+let _origBgInit = null;
+let _patchCount = 0;
+const _allConns = [];
+
+function _connectAdjustment(adj) {
+    const id = adj.connect('notify::value', () => {
+        const val = adj.value;
+        const newOpacity = Math.round(255 * (1 - val));
+        console.warn(`[LIDSoL] adj.value=${val} opacity=${newOpacity}`);
+        for (const w of _widgets)
+            w.opacity = newOpacity;
+    });
+    _allConns.push([adj, id]);
+}
+
+function _walkAndConnect(actor, visited) {
+    if (actor._stateAdjustment && !visited.has(actor._stateAdjustment)) {
+        visited.add(actor._stateAdjustment);
+        _connectAdjustment(actor._stateAdjustment);
+    }
+    if (typeof actor.foreach === 'function')
+        actor.foreach(child => _walkAndConnect(child, visited));
+    else if (actor.get_children)
+        actor.get_children().forEach(child => _walkAndConnect(child, visited));
+}
+
+function _patchBgInit() {
+    if (_origBgInit)
+        return;
+    const bgGroup = Main.layoutManager._backgroundGroup;
+    if (bgGroup)
+        _walkAndConnect(bgGroup, new Set());
+    _origBgInit = WorkspaceBackground.prototype._init;
+    WorkspaceBackground.prototype._init = function (...args) {
+        _origBgInit.call(this, ...args);
+        if (this._stateAdjustment)
+            _connectAdjustment(this._stateAdjustment);
+    };
+}
+
+function _unpatchBgInit() {
+    for (const [adj, id] of _allConns)
+        adj.disconnect(id);
+    _allConns.length = 0;
+    if (_origBgInit) {
+        WorkspaceBackground.prototype._init = _origBgInit;
+        _origBgInit = null;
+    }
+}
 
 const DesktopWidget = GObject.registerClass(
 class DesktopWidget extends St.Widget {
@@ -10,40 +63,52 @@ class DesktopWidget extends St.Widget {
         super._init({ reactive: false });
 
         this._settings = settings;
+        this._raiseIdleId = 0;
 
-        this._addToBackgroundGroup();
+        Main.layoutManager.uiGroup.add_child(this);
+        this._raiseToTop();
         this._setupOverviewFade();
+
+        if (Main.overview.visible)
+            this.opacity = 0;
 
         this.connect('destroy', () => {
             this._removeOverviewFade();
+            this._cancelRaiseIdle();
         });
     }
 
-    _addToBackgroundGroup() {
-        const bgGroup = Main.layoutManager._backgroundGroup;
-        const container = bgGroup || Main.layoutManager.uiGroup;
-        container.add_child(this);
+    _raiseToTop() {
+        this._cancelRaiseIdle();
+        this._raiseIdleId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+            this._raiseIdleId = 0;
+            const parent = this.get_parent();
+            if (parent)
+                parent.set_child_above_sibling(this, null);
+            return GLib.SOURCE_REMOVE;
+        });
+    }
+
+    _cancelRaiseIdle() {
+        if (this._raiseIdleId) {
+            GLib.source_remove(this._raiseIdleId);
+            this._raiseIdleId = 0;
+        }
     }
 
     _setupOverviewFade() {
-        const controls = Main.overview?._overview?._controls;
-        if (!controls)
-            return;
-
-        this._stateAdjustment = controls._stateAdjustment;
-        if (!this._stateAdjustment)
-            return;
-
-        this._stateSignalId = this._stateAdjustment.connect('notify::value', () => {
-            this.opacity = 255 * (1 - this._stateAdjustment.value);
-        });
+        _widgets.add(this);
+        _patchCount++;
+        if (_patchCount === 1)
+            _patchBgInit();
     }
 
     _removeOverviewFade() {
-        if (this._stateSignalId && this._stateAdjustment) {
-            this._stateAdjustment.disconnect(this._stateSignalId);
-            this._stateSignalId = 0;
-            this._stateAdjustment = null;
+        _widgets.delete(this);
+        _patchCount--;
+        if (_patchCount <= 0) {
+            _patchCount = 0;
+            _unpatchBgInit();
         }
     }
 });
